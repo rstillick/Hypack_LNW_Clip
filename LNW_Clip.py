2e78394a66405edca4cd6c5d21855db86298c3cd
Roger D. Stillick Jr <rstillick@gmail.com>
2024-12-08 18:19:58 -0600

    Fixed detect_bend(reach, total_lines) function. Fixed output file name.

--- LNW_Clip.py
+++ LNW_Clip.py
@@ -17,14 +17,14 @@ def main():
     # Initialization variables
     WorkingDirectory = "/home/roger/Documents/Python/LNW_Clip/"
     lnw_file = "GI_02_HIB.lnw"
-    lnw_out_file = lnw_file.split(',')
+    lnw_out_file = lnw_file.split('.')
     print(f"lnw_out_file is {lnw_file}")
     lnw_path = WorkingDirectory + lnw_file
 
     lnw_out_path = WorkingDirectory + lnw_out_file[0] + "_out.lnw"
     print(f"line output path is {lnw_out_path}")
-    start_line = "170+000"
-    end_line = "190+000"
+    start_line = "162+000"
+    end_line = "320+000"
 
 
     # step 1
@@ -40,15 +40,20 @@ def main():
 
     # step 3
     reach_bends = detect_bend(reach, total_lines)
+    # print statement for testing
+    print(reach_bends)
 
     # step 4 remove odd lines
-    reach_copy = (remove_odds(reach))
+    #reach_copy = (remove_odds(reach))
+    reach = remove_odds(reach)
+    # print statement for testing
+    # print(reach)
+    reach = remove_cs(reach, reach_bends)
 
-    #print(reach_bends)
-    # need to
-    total_lines = count_total_lines(reach_copy)
+    total_lines = count_total_lines(reach)
     # write the lines to a new LNW file
-    write_lnw(reach_copy, total_lines, lnw_out_path)
+    write_lnw(reach, total_lines, lnw_out_path)
+    #write_lnw(reach_bends, total_lines, lnw_out_path)
 
 def load_lnw(lnw_path):
 
@@ -99,9 +104,8 @@ def detect_bend(reach, total_lines):
         #print(f"line count {count}")
         #print(f"x is {x}")
         bearing = reach[x].split(" ")
-        #print(bearing[1])
+        print(bearing[1])
 
-        # Why is this getting an out of bounds error?
         if count +16 < len(reach):
             next_bearing = reach[x + 16].split(" ")
             #print (f"next bearing {next_bearing[1]}")
@@ -120,45 +124,64 @@ def detect_bend(reach, total_lines):
 
             else:
                 bend_count = bend_count + 1
-                reach_bends.append(reach[x- 7])
+                reach_bends.append(reach[x-8])
+                #reach_bends.append(bearing)
+                #reach_bends.append(bearing[0]+bearing[1]+bearing[2])
+                #reach_bends.append(reach[x-8])
 
                 #print(f"count of bend: {bend_count}")
-                #print("bend")
+                #print(f"bend {bearing}")
         else:
             exit
 
         previous_bearing = bearing
     print("found all bends")
-    return reach_bends
+    print(reach_bends)
+    return(reach_bends)
 
 def remove_odds(reach):
     # remove odd ending lines (100, 300, 500, 700, 900)
-    reach_copy = reach
+    #reach_copy = reach
     odds = {'100', '300', '500', '700', '900'}
 
-    for index, line in enumerate(reach_copy):
+    for index, line in enumerate(reach):
 
         if line[0:3] == "LNN":
             line_split = line.split("+")
-            print (line_split[1])
+            # print statement for testing
+            #print (line_split[1])
 
             if line_split[1] in odds:
                 print(f"Deleting odd line {str(line_split[0])}+{str(line_split[1])}")
-                del reach_copy[index -3: index +13]
+                del reach[index -3: index +13]
 
     #print statement for testing
     #print(reach_copy)
 
-    return(reach_copy)
+    return(reach)
 
 def remove_cs(reach, reach_bends):
-    # only remove lines that end with 100, 200, 300, 500, 700 800, 900.
-    # if lines are on a bend, remove odd lines (100, 300, 500, 700, 900).
-    pass
+    # Removes the extra lines in straight sections that are not needed for conditional surveys.
+    # This is to be run after the function to remove_odds(reach).
+    # Removes lines that end in 200 or 800.
 
+    cs_not = {'200', '800'}
 
+    for index, line in enumerate(reach):
 
+        if line[0:3] == "LNN":
+            line_split = line.split("+")
+            # print statement for testing
+            # print (line_split[1])
 
+            if line not in reach_bends and line_split[1] in cs_not:
+                print(f"Deleting line {str(line_split[0])}+{str(line_split[1])}")
+                del reach[index - 3: index + 13]
+                #if line_split[1] in cs_not:
+                #    print(f"Deleting line {str(line_split[0])}+{str(line_split[1])}")
+                #    del reach[index - 3: index + 13]
+
+    return(reach)
 
 def write_lnw(reach, total_lines, lnw_out_path):
 
